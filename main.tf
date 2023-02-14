d673b204fb411ab0a0558cef4dd0a6eff98ab3b9
AjeethP <85919807+AjeethP@users.noreply.github.com>
2023-02-14 08:08:28 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 
 
 module "IAM" {
-    source = "git::https://AjeethP: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@github.com/AjeethP/test-module-1.git//test/3.0?ref=v10"
+    source = "./test-module-1.git/test/3.0"
 
    role_name = "test-role-mod-1"# Name of the role
    application_id = "POC"# Application id
